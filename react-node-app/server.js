069ad0b6716adc93f4cb509176fa4bcf91d05f1d
Rid <shakeel.ridhwaan@gmail.com>
2018-04-06 20:59:37 -0400

    cut down

--- react-node-app/server.js
+++ react-node-app/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.get('/api/hello', (req, res) => {
+app.get('/hello', (req, res) => {
   let hello = { node-app: 'Hello World'};
   res.json(hello);
 });
