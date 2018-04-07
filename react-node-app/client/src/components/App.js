bd7b830efeea94e77ca94d2f45758f34d47bbfa3
Rid <shakeel.ridhwaan@gmail.com>
2018-04-06 20:55:32 -0400

    react-node-app

---
+++ react-node-app/client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+        	<p id="hello">Hello world</p>
+        </div>
+      )
+  }
+}
+
+export default App;
---
+++ react-node-app/server.js
@@ -0,0 +1,18 @@
+var _ = require('underscore');
+var figlet = require('figlet');
+var fs = require('fs');
+var node_dir = require('node-dir');
+var bluebird = require('bluebird');
+var yaml = require('js-yaml');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/api/hello', (req, res) => {
+  let hello = { node-app: 'Hello World'};
+  res.json(hello);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
